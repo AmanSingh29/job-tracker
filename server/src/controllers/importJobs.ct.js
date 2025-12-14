b4f111ac5269208f2da20f2cc93bf5d050241cb8
Aman Singh <amansingh979429@gmail.com>
2025-12-14 15:03:58 +0530

    response changes

--- server/src/controllers/importJobs.ct.js
+++ server/src/controllers/importJobs.ct.js
@@ -2,7 +2,7 @@ const JobImportService = require("../services/importJobs.service");
 
 exports.runImport = async (req, res, next) => {
   const feed_url = "https://jobicy.com/?feed=job_feed";
-  const result = JobImportService.importFromFeed(feed_url);
+  const result = await JobImportService.importFromFeed(feed_url);
   res.data = result;
   next();
 };
